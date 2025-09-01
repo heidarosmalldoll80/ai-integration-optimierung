c08dcb1c8fc0498ad7694459d805ab44a64fa40d
heidarosmalldoll80 <riyensarahh@hotmail.com>
2025-09-01 10:37:26 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>AI-Integration Optimierung</h1>
+      <p>Willkommen in der Anwendung zur Künstlichen Intelligenz Integration!</p>
+    </div>
+  );
+}
+
+export default App;
